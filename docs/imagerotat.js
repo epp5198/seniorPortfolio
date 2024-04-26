c91f2bd98832e52e6c9638e5a8cbc2c1cb45b007
egan <epp5198@psu.edu>
2024-04-25 23:59:35 -0400

    please work

--- docs/imagerotat.js
+++ docs/imagerotat.js
@@ -6,7 +6,7 @@ var thisImage = 0;
 /*ebb: This javascript rotates images on reload from server*/
 
 function rotate() {
-    var images = new Array ('images/treewaterfall-sm.png', 'images/waterfallclose.png', 'images/waterfallonly.png', 'images/waterfalltreebw.png', 'images/partialeclipsesplitclouds.png', 'images/solarunion.png', 'images/totaleclipse.png', 'images/totaleclipseclose.png')
+    var images = new Array ('images/treewaterfall-sm.png', 'images/waterfallclose.png', 'images/waterfallonly.png', 'images/waterfalltreebw.png', 'images/partialeclipsesplitclouds.png', 'images/solarunion.png', 'images/totaleclipse.png', 'images/totaleclipseclose.png','images/bird.png', 'images/jellyfish.png', 'images/skihill.png', 'images/leafs.png', 'images/boatbeach.png', 'images/splash.png', 'images/reflect.png', 'images/wateroverrocks.png', 'images/falltrees.png', 'images/stars.png', 'images/dipperdots.png', 'images/colordipper.png', 'images/beach.png', 'images/boats.png', 'images/cloud.png', 'images/sunsetlake.png', 'images/moss.png', 'images/bubbles.png', 'images/waterfall.png', 'images/shroomtree.png', 'images/shroomstump.png', 'images/waterfallhigh.png', 'images/ttw.png', 'images/treeframe.png', 'images/treewaterfallrv.png', 'images/trees.png')
     
     /*ebb: This line randomizes the order of the images:*/
     var thisImage = Math.floor(Math.random() *(images.length));
